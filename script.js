c9886140560024eba1472b4366d5bfa3edaf9c8a
Jayden <335670428@gapps.yrdsb.ca>
2023-05-16 16:23:48 -0400

    Enter to add task and ability for empty task removed

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 function addweekly() {
     var input = document.getElementById('input').value;
-    const task = document.createTextNode(input);
-    var listItem = document.createElement('li');
-        var inp = document.createElement('input');
-        inp.setAttribute('type', 'checkbox');
-        inp.setAttribute('onclick', 'removeItem(this)');
+    if (input == ""){}
+    else {
+        const task = document.createTextNode(input);
+        var listItem = document.createElement('li');
+            var inp = document.createElement('input');
+            inp.setAttribute('type', 'checkbox');
+            inp.setAttribute('onclick', 'removeItem(this)');
 
-            var text = document.createElement('p');
-            text.appendChild(task)
+                var text = document.createElement('p');
+                text.appendChild(task);
 
-        listItem.appendChild(inp)
-        listItem.appendChild(text)
-    document.getElementById("addtskbtn").appendChild(listItem);
-
+            listItem.appendChild(inp);
+            listItem.appendChild(text);
+        document.getElementById("addtskbtn").appendChild(listItem);
+    }
 
 
 }
+
+function enterKeyPressed(event) {
+    if (event.keyCode == 13) {
+       addweekly()
+       return true;
+    } else {
+       return false;
+    }
+ }
 
 function removeItem(checkbox) {
     var listItem = checkbox.parentNode;
